c7fe0c52c4bc3297077b1613af5f801815508feb
Herr Diesel <64426028+HerrDiesel@users.noreply.github.com>
2023-04-21 15:38:21 +0200

    Add 'read' function

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,1 @@
-
+export function read(sygn_akt: string, court: string): any;
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const courts = {
+    "NSA": require("./courts/js/nsa.js"),
+    "WSA": require("./courts/js/wsa.js"),
 
+    "SN": require("./courts/js/sn.js"),
+    "SA": require("./courts/js/sa.js"),
+    "SO": require("./courts/js/so.js"),
+    "SR": require("./courts/js/sr.js")
+};
+
+function read(sygn_akt, court){
+    if(courts[court] !== undefined){
+        courts[court].read(sygn_akt);
+    }
+    else{
+        recognize(sygn_akt);
+    }
+}
+
+function recognize(sygn_akt){
+    // work in progress
+}
+
+// MODULE EXPORTS
+module.export.read = read;
