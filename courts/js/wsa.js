ef15654c98769835e13e1e2f8c32b05c52c623bf
Herr Diesel <64426028+HerrDiesel@users.noreply.github.com>
2023-04-22 12:10:05 +0200

    Fix typos and change code formatting a bit

--- courts/js/wsa.js
+++ courts/js/wsa.js
@@ -11,7 +11,7 @@ function read(input){
     output.repertorium = output.repertorium.split("/")[0];
 
     output.city.abbr = sygn_akt[1];
-    output.city.abbr = output.city.split("/")[1];
+    output.city.abbr = output.city.abbr.split("/")[1];
     
     output.num = sygn_akt[2];
     output.num = output.num.split("/")[0];
@@ -32,11 +32,10 @@ function read(input){
     }
 
     // Identify the city
-    const cities = require("../json/wsa.loc.json");
-    const city = cities.find(c => c.abbr === output.city.abbr);
-    
-    output.city.en_name = city["en_name"];
-    output.city.pl_name = city["pl_name"];
+    const cities = require("../json/wsa.cities.json");
+
+    output.city.en_name = cities.find(c => c.abbr === output.city.abbr)["en_name"];
+    output.city.pl_name = cities.find(c => c.abbr === output.city.abbr)["pl_name"];
 
     return output;
 }
@@ -45,17 +44,17 @@ function read(input){
 function generate(division, repertorium, city, num, year){
     // Identify the city
     if(city.length != 2){
-        const cities = require("../json/wsa.loc.json");
+        const cities = require("../json/wsa.cities.json");
         if(cities.find(c => c.en_name === city) === undefined){
             if(cities.find(c => c.pl_name === city) === undefined){
                 throw new Error("wrong city identifier");
             }
             else{
-                city = cities.find(c => c.pl_name === city)["abbv"]; 
+                city = cities.find(c => c.pl_name === city)["abbr"]; 
             }
         }
         else{
-            city = cities.find(c => c.en_name === city)["abbv"];
+            city = cities.find(c => c.en_name === city)["abbr"];
         }
     }
 
